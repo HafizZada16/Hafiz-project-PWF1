37c1c09a431a21a4c125f734401be6febce67b17
HafizZada16 <hafizzada16@gmail.com>
2026-02-21 15:30:19 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 1 - Laravel 12</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-black flex items-center justify-center min-h-screen">
+
+    <div class="border border-gray-700 rounded-lg p-20 w-3/4 max-w-4xl text-left">
+        <div class="text-white space-y-2">
+            <h1 class="text-xl font-medium">Hafiz Zada Maulana</h1>
+            <p class="text-gray-400">20230140016</p>
+            
+            <button class="mt-4 bg-white text-black px-6 py-2 rounded-md font-medium hover:bg-gray-200 transition">
+                Modul Pertemuan 1
+            </button>
+        </div>
+    </div>
+
+</body>
+</html>
